84f61ddf37bb51f2e893427bb1ae6f6f867c6ecd
unknown <hamzeh.alnatshehgmail.com>
2022-02-04 20:57:01 +0300

    new deign

--- views/site/achievements.php
+++ views/site/achievements.php
@@ -4,12 +4,12 @@ $this->title = $achievement->title ;
 <div id="achievements" class="board">
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-12 col-lg-5">
                 <div>
                     <img src="<?= Yii::$app->request->baseUrl . '/' . $achievement->image  ?>" width="100%" height="350px" />
                 </div>
             </div>
-            <div class="col-7">
+            <div class="col-12 col-lg-7">
                 <div>
                     <h1 class="course-title">
                         <strong>
